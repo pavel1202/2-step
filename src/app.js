ac91c5baf40f7e0a0937b25638013a7484629aa0
pavel1202 <pavel2275@yandex.ru>
2020-06-08 15:29:21 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import './style.css';
+import Icon from './system-shutdown-th.png';
+import './style.scss';
+import Icon1 from'./img/Logo.png';
+import './img/pointer.png';
+import './img/messengers/facebook.png';
+import './img/messengers/instagram.png';
+import './img/messengers/twitter.png';
+import './img/arrow.png';
