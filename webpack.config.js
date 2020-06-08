1af252ce476dd60b29244fa90bbaa5b70ff7e808
pavel1202 <pavel2275@yandex.ru>
2020-06-08 15:31:48 +0300

    first commit1

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+
+const config = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+      template: './src/pages/index.pug'
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'ap.html',
+        template: './src/pages/ap.pug'
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'HeadersFooters.html',
+        template: './src/pages/HeadersFooters.pug'
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'Cards.html',
+        template: './src/pages/Cards.pug'
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'FormElements.html',
+        template: './src/pages/FormElements.pug'
+    }),
+  ],
+  module: {
+    rules: [
+        { 
+        test: /\.pug$/,
+        use: ["pug-loader"]
+        },
+        {
+        test: /\.css$/,
+        use: [
+           'style-loader',
+           'css-loader',
+          ],
+        },
+        {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                outputPath:'./img',
+                name:'[name].[ext]'
+            }
+        }],
+        },
+        {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                outputPath:'./fonts',
+                name:'[name].[ext]'
+            }
+        }],
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          },
+        
+    ]
+  }
+};
+module.exports = (env, argv) => {
+if (argv.mode === 'development') {}
+ if (argv.mode === 'production') {}
+return config;
+}
